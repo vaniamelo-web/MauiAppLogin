23bbb672937cc0054598961685d13c21beb92638
vaniamelo-web <vanvanmelo2796@gmail.com>
2026-05-16 09:55:09 -0300

    Add arquivos para o Git

--- MauiAppLogin/App.xaml.cs
+++ MauiAppLogin/App.xaml.cs
@@ -7,11 +7,14 @@ namespace MauiAppLogin
         public App()
         {
             InitializeComponent();
+            MainPage = new AppShell();
         }
-
-        protected override Window CreateWindow(IActivationState? activationState)
+        protected override Window CreateWindow(IActivationState activationState)
         {
-            return new Window(new AppShell());
+            var window = base.CreateWindow(activationState);
+            window.Width = 400;
+            window.Height = 600;
+            return window;
         }
-    }
-}
+    } 
+} 
